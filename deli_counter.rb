d6d966ad96f0441246218950286dafc4b94e0279
chejianchao <820992376@qq.com>
2018-07-06 03:08:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,24 +3,26 @@
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  "Welcome, Ada. You are number #{katz_deli.size} in line."
+  puts "Welcome, Ada. You are number #{katz_deli.size} in line."
 end
 
 def line(deli)
   if deli.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
+    return
   end
   res = "The line is currently:"
   deli.each_with_index do |name, idx|
     res+=" #{idx+1}. #{name}"
   end
-  return res
+  puts res
 end
 
 def now_serving(deli)
   if deli.size == 0 
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
+    return
   end
   name = deli.pop()
-  "Currently serving #{name}"
+  puts "Currently serving #{name}"
 end
